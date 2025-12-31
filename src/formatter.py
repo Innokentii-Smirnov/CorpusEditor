291e19d452ea4a94df033867fc84ded9cfa882ef
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-31 13:18:04 +0300

    Improve typing

--- src/formatter.py
+++ src/formatter.py
@@ -1,10 +1,11 @@
 from bs4.formatter import XMLFormatter
 from bs4 import Tag
+from bs4.element import AttributeValueList
 from collections.abc import Iterable
 
 class CustomFormatter(XMLFormatter):
 
-  def attributes(self, tag: Tag) -> Iterable[tuple[str, str]]:
+  def attributes(self, tag: Tag) -> Iterable[tuple[str, str | AttributeValueList]]:
     """Return the attributes in the original order.
 
     :param tag: An object representing an XML tag.
