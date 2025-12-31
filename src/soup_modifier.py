e6f73cda5ed7e24baf5eeba633e24588a6cefdd9
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-12-31 16:46:29 +0300

    Implement morph. analysis splitting (replacement with multiple new analyses)

--- src/soup_modifier.py
+++ src/soup_modifier.py
@@ -39,12 +39,19 @@ class SoupModifier:
                 else:
                     logger.warning('Line {0} in {1} is not marked for language.'.format(lnr, rel_name))
                     lang = 'Hur'
-            elif tag.name == 'w' and lang == 'Hur':
-                if 'lg' in tag.attrs and tag['lg'] != 'Hur':
-                    continue
+            elif tag.name == 'w' and (lang == 'Hur' or
+                                      'lg' in tag.attrs and tag['lg'] == 'Hur'):
                 if 'mrpnan' in tag.attrs:
                     del tag.attrs['mrpnan']
+                free_index = 1
                 for attr, value in tag.attrs.items():
+                    if attr.startswith('mrp') and attr != 'mrp0sel':
+                        index_str = attr.removeprefix('mrp')
+                        if index_str.isdigit():
+                            current_index = int(index_str)
+                            if current_index >= free_index:
+                                free_index = current_index + 1
+                for attr, value in list(tag.attrs.items()):
                     if attr.startswith('mrp') and attr != 'mrp0sel':
                         try:
                           if isinstance(value, str):
@@ -58,7 +65,8 @@ class SoupModifier:
                         if morph is None:
                           morph_logger.error('The following morphological analysis could not be parsed:\n%s on line %s in %s', value, lnr, rel_name)
                         elif morph in self.changes:
-                            replacement = self.changes[morph][0]
+                            replacements = self.changes[morph]
+                            replacement = replacements[0]
                             if isinstance(morph, MultiMorph):
                                 index = next(iter(morph.morph_tags))
                                 replacement = replacement.to_multi(index)
@@ -67,7 +75,13 @@ class SoupModifier:
                             if not modified:
                                 logger.info(rel_name)
                             modified = True
-                            logger.info(
-                                '\t{0:10} {1:45} {2}'.format(lnr, value, repl_str)
-                            )
+                            logger.info('\t{0}'.format(lnr))
+                            logger.info('\t\t{0} =>'.format(value))
+                            logger.info('\t\t{0}'.format(repl_str))
+                            for replacement in replacements[1:]:
+                                attr = 'mrp' + str(free_index)
+                                repl_str = replacement.__str__()
+                                tag[attr] = repl_str
+                                free_index += 1
+                                logger.info('\t\t{0}'.format(repl_str))
         return modified
