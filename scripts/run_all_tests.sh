0c4c409b5020e9177ea0fa4912e7f5509337507a
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-03 17:30:20 +0300

    Move old output removal to the single test script

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,6 +1,6 @@
 script_dir=$(dirname "$0")
 directory="$1"
-"$script_dir"/clear.sh "$directory"
+#"$script_dir"/clear.sh "$directory"
 for name in $(ls "$directory"); do
   test="$directory/$name"
   if [ -d "$test" ]; then
--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,5 +1,9 @@
 script_dir=$(dirname "$0")
 test="$1"
+output="$test/output"
+if [ -d "$output" ]; then
+  rm -r "$output"
+fi
 "$script_dir"/make_config.sh "$test" "input" "output" > config.json
 ~/environment2/bin/python ~/CorpusEditor/src/edit_corpus.py
 rm config.json
