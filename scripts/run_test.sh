1435d65abfe8b336a01dda8690c9abf24a36fb58
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-01-03 12:34:56 +0300

    Pass parameters to the config-generation scripts, remove config at the end of a test

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,5 +1,6 @@
 script_dir=$(dirname "$0")
 test="$1"
-"$script_dir"/make_config.sh "$test" > config.json
+"$script_dir"/make_config.sh "$test" "input" "output" > config.json
 ~/environment2/bin/python ~/CorpusEditor/src/edit_corpus.py
+rm config.json
 diff -r "$test/expected" "$test/output"
